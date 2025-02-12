36cab96f23152aa8395036154664c7956432d210
Danuzu11 <davidalejandrp74@gmail.com>
2025-02-12 00:51:21 -0400

    first commit Boton de pausa flappy

---
+++ 02-flappy_bird/src/states/PauseState.cpp
@@ -0,0 +1,49 @@
+/*
+    ISPPV1 2023
+    Study Case: Flappy Bird
+
+    Author: Alejandro Mujica
+    alejandro.j.mujic4@gmail.com
+
+    This file contains the definition of the class PlayingBaseState.
+*/
+
+#include <Settings.hpp>
+#include <src/text_utilities.hpp>
+#include <src/states/StateMachine.hpp>
+#include <src/states/PauseState.hpp>
+
+PauseState::PauseState(StateMachine* sm) noexcept
+    : BaseState{sm}
+{
+
+}
+
+void PauseState::enter(std::shared_ptr<World> _world, std::shared_ptr<Bird> _bird) noexcept
+{
+    world = _world;
+    bird = _bird;
+    world->stop();
+    bird->stop();
+}
+
+void PauseState::handle_inputs(const sf::Event& event) noexcept
+{
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
+    {
+        state_machine->change_state("playing",world,bird);
+    }
+}
+
+void PauseState::update(float dt) noexcept
+{
+    // There is no updates in pause state
+}
+
+void PauseState::render(sf::RenderTarget& target) const noexcept
+{
+    world->render(target);
+    bird->render(target);
+    render_text(target, Settings::VIRTUAL_WIDTH / 2, Settings::VIRTUAL_HEIGHT / 3, "PAUSE ", Settings::FLAPPY_TEXT_SIZE, "flappy", sf::Color::White,true);
+    render_text(target, Settings::VIRTUAL_WIDTH / 2, 2 * Settings::VIRTUAL_HEIGHT / 3, "Press 'P' to continue.... ", Settings::MEDIUM_TEXT_SIZE, "font", sf::Color::White,true);
+}
---
+++ 02-flappy_bird/src/states/PlayingState.cpp
@@ -0,0 +1,87 @@
+/*
+    ISPPV1 2023
+    Study Case: Flappy Bird
+
+    Author: Alejandro Mujica
+    alejandro.j.mujic4@gmail.com
+
+    This file contains the definition of the class PlayingBaseState.
+*/
+
+#include <Settings.hpp>
+#include <src/text_utilities.hpp>
+#include <src/states/StateMachine.hpp>
+#include <src/states/PlayingState.hpp>
+
+PlayingState::PlayingState(StateMachine* sm) noexcept
+    : BaseState{sm}
+{
+
+}
+
+void PlayingState::enter(std::shared_ptr<World> _world, std::shared_ptr<Bird> _bird) noexcept
+{
+    world = _world;
+    // nuevo
+    world->start();
+    world->reset(true);
+    
+    if (_bird == nullptr)
+    {
+        bird = std::make_shared<Bird>(
+            Settings::VIRTUAL_WIDTH / 2 - Settings::BIRD_WIDTH / 2, Settings::VIRTUAL_HEIGHT / 2 - Settings::BIRD_HEIGHT / 2,
+            Settings::BIRD_WIDTH, Settings::BIRD_HEIGHT
+        );
+    }
+    else
+    {
+        bird = _bird;
+        // nuevo
+        bird->start();
+    }
+
+    //nuevo SABRA DIOS PORQUE FUNCIONO JE JE TIRE LA FLECHA Y MATE A CUPIDO
+    score = world->get_global_score();
+
+}
+
+void PlayingState::handle_inputs(const sf::Event& event) noexcept
+{
+    if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
+    {
+        bird->jump();
+    }
+    //NUEVO
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
+    {
+        world->set_global_score(score);
+        afterpause = true;
+        state_machine->change_state("pause",world,bird);
+    }
+}
+
+void PlayingState::update(float dt) noexcept
+{
+    bird->update(dt);
+    world->update(dt);
+
+    if (world->collides(bird->get_collision_rect()))
+    {
+        Settings::sounds["explosion"].play();
+        Settings::sounds["hurt"].play();
+        state_machine->change_state("count_down");
+    }
+
+    if (world->update_scored(bird->get_collision_rect()))
+    {
+        ++score;
+        Settings::sounds["score"].play();
+    }
+}
+
+void PlayingState::render(sf::RenderTarget& target) const noexcept
+{
+    world->render(target);
+    bird->render(target);
+    render_text(target, 20, 10, "Score: " + std::to_string(score), Settings::FLAPPY_TEXT_SIZE, "flappy", sf::Color::White);
+}
